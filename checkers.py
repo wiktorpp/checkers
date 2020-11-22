ffb3887634df4276b7c4812df0129655b6389c89
wiktorpp <grazynapp65@gmail.com>
2020-11-22 20:27:45 +0000

    first commit

---
+++ checkers.py
@@ -0,0 +1,41 @@
+from sys import *
+
+startBoard = [
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [2, 0, 2, 0, 2, 0, 2, 0],
+    [0, 2, 0, 2, 0, 2, 0, 2],
+    [2, 0, 2, 0, 2, 0, 2, 0]
+]
+
+def asciifyBoard(board):
+    ret = ""
+    ret += "╔═╦═╦═╦═╦═╦═╦═╦═╗\n"
+    for i in board:
+        ret += "║"
+        for j in i:
+            if j == 0:
+                ret += " "
+            if j == 1:
+                ret += "1"
+            if j == 2:
+                ret += "2"
+            ret += "│"
+        ret = ret[:-1]
+        ret += "║\n╠"
+        for j in range(len(i)):
+            ret += "─┼"
+        ret = ret[:-1]
+        ret += "╣\n"
+    ret = ret[:-18]
+    ret += "╚"
+    for j in range(len(i)):
+        ret += "═╩"
+    ret = ret[:-1]
+    ret += "╝"
+    return ret
+
+print(asciifyBoard(startBoard))
